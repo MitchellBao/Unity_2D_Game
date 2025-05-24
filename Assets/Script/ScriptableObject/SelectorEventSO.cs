8b287194568959ea51a32e334a6e501345491e7f
MitchellBao <mitchellbao29@gmail.com>
2025-05-24 19:04:49 +0800

    help panel finished

---
+++ Assets/Script/ScriptableObject/SelectorEventSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorEventSO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
